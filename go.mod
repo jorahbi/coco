b33d0032d815402b526d507f69ec5e12e687db10
ice <ibibin@sina.com>
2023-04-20 08:14:46 +0800

    xorm

--- go.mod
+++ go.mod
@@ -2,16 +2,35 @@ module github.com/jorahbi/coco
 
 go 1.20
 
-require github.com/zeromicro/go-zero v1.5.1
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/xormplus/xorm v0.0.0-20210822100304-4e1d4fcc1e67
+	github.com/zeromicro/go-zero v1.5.1
+	golang.org/x/sync v0.1.0
+)
 
 require (
+	github.com/Chronokeeper/anyxml v0.0.0-20160530174208-54457d8e98c6 // indirect
+	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/CloudyKit/jet v2.1.2+incompatible // indirect
+	github.com/agrison/go-tablib v0.0.0-20160310143025-4930582c22ee // indirect
+	github.com/agrison/mxj v0.0.0-20160310142625-1269f8afb3b4 // indirect
+	github.com/bndr/gotabulate v1.1.2 // indirect
+	github.com/clbanning/mxj v1.8.4 // indirect
 	github.com/fatih/color v1.15.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/tealeg/xlsx v1.0.5 // indirect
+	github.com/xormplus/builder v0.0.0-20200331055651-240ff40009be // indirect
 	go.opentelemetry.io/otel v1.14.0 // indirect
 	go.opentelemetry.io/otel/trace v1.14.0 // indirect
 	go.uber.org/automaxprocs v1.5.2 // indirect
@@ -19,5 +38,6 @@ require (
 	google.golang.org/genproto v0.0.0-20230123190316-2c411cf9d197 // indirect
 	google.golang.org/grpc v1.54.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/flosch/pongo2.v3 v3.0.0-20141028000813-5e81b817a0c4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ xormplusx/xorm.go
@@ -0,0 +1,120 @@
+package xormplusx
+
+import (
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/xormplus/xorm"
+	"golang.org/x/sync/singleflight"
+)
+
+const (
+	UNDELETED = 0
+	DELETED   = 1
+	INSERT    = "insert"
+	UPDATE    = "update"
+)
+
+type engineGroup struct {
+	group *xorm.EngineGroup
+}
+
+type Options struct {
+	Sources struct {
+		Master string
+		Slave  []string
+	}
+	ShowSQL     bool
+	StplPath    string
+	MaxOpen     int
+	MaxIdle     int
+	MaxLifetime int
+}
+
+type SqlArgs map[string]any
+
+var single = &singleflight.Group{}
+
+func OrmGroup(o Options) *engineGroup {
+	group, err, _ := single.Do("engine_group", func() (interface{}, error) {
+		return newOrm(o)
+	})
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	return group.(*engineGroup)
+}
+
+func newOrm(o Options) (*engineGroup, error) {
+	var (
+		slaves        []*xorm.Engine
+		master, slave *xorm.Engine
+		group         *xorm.EngineGroup
+		err           error
+	)
+	orm := &engineGroup{}
+	if master, err = xorm.NewEngine("mysql", o.Sources.Master); err != nil {
+		return orm, err
+	}
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	for _, source := range o.Sources.Slave {
+		if slave, err = xorm.NewEngine("mysql", source); err != nil {
+			return orm, err
+		}
+		slaves = append(slaves, slave)
+	}
+
+	if group, err = xorm.NewEngineGroup(master, slaves); err != nil {
+		return orm, err
+	}
+	if err = group.Ping(); err != nil {
+		return orm, err
+	}
+	if err = group.RegisterSqlTemplate(xorm.Pongo2(o.StplPath, ".stpl")); err != nil {
+		return orm, err
+	}
+	group.StartFSWatcher()
+	group.ShowSQL(o.ShowSQL)
+
+	// 连接池中最大连接数
+	group.SetMaxOpenConns(100)
+	// 连接池中最大空闲连接数
+	group.SetMaxIdleConns(10)
+	// 单个连接最大存活时间(单位:秒)
+	group.SetConnMaxLifetime(10000)
+
+	return orm, nil
+
+}
+
+func (orm *engineGroup) NewSqlArgs(queryId string) SqlArgs {
+	args := SqlArgs{}
+	args.Set("queryId", queryId)
+	args.Set("isDelete", UNDELETED)
+	return args
+}
+
+func (args SqlArgs) Set(key string, value any) SqlArgs {
+	args[key] = value
+	return args
+}
+
+func (args SqlArgs) Done() map[string]any {
+	return map[string]any(args)
+}
+
+func (args SqlArgs) String() string {
+	return fmt.Sprintln("sql args", map[string]any(args))
+}
+
+func (orm *engineGroup) ReadConn() *xorm.Engine {
+	return orm.group.Main()
+}
+
+func (orm *engineGroup) WriteConn() *xorm.Engine {
+	return orm.group.Subordinate()
+}
