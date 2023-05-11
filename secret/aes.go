0b8a96b865fa28d1a9b606236c8ec3945f0f315a
ice <ibibin@sina.com>
2023-05-12 00:45:19 +0800

    aes

--- secret/aes.go
+++ secret/aes.go
@@ -1,68 +1,35 @@
 package secret
 
 import (
-	"bytes"
 	"crypto/aes"
-	"crypto/cipher"
-	"encoding/base64"
+	"encoding/hex"
 )
 
-// aes加密
-func AesEncrypt(plantText, key []byte) (string, error) {
-	// 这个代码比较重要，是涉及到签名大小
-	if len(key) > 16 {
-		key = key[:16]
-	}
-	//NewCipher该函数限制了输入k的长度必须为16, 24或者32l
-	block, err := aes.NewCipher(key)
+func EncryptAES(key []byte, plaintext string) (string, error) {
+
+	c, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
-	//补全码
-	plantText = pkcs7Padding(plantText, block.BlockSize())
-	//加密模式
-	blockModel := cipher.NewCBCEncrypter(block, key[:block.BlockSize()])
-	//创建数组
-	ciphertext := make([]byte, len(plantText))
-	//加密
-	blockModel.CryptBlocks(ciphertext, plantText)
 
-	return base64.StdEncoding.EncodeToString(ciphertext), nil
+	out := make([]byte, len(plaintext))
+
+	c.Encrypt(out, []byte(plaintext))
+
+	return hex.EncodeToString(out), nil
 }
 
-func pkcs7Padding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padtext...)
-}
+func DecryptAES(key []byte, ct string) (string, error) {
+	ciphertext, _ := hex.DecodeString(ct)
 
-// aes解密
-func AesDecrypt(ciphertext string, key []byte) ([]byte, error) {
-	cipherbyte, err := base64.StdEncoding.DecodeString(ciphertext)
+	c, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	// 这个代码比较重要，是涉及到签名大小
-	if len(key) > 16 {
-		key = key[:16]
-	}
-	//分组秘钥
-	block, err := aes.NewCipher(key) // 选择加密算法
-	if err != nil {
-		return nil, err
-	}
-	//加密模式
-	blockModel := cipher.NewCBCDecrypter(block, key[:block.BlockSize()])
-	plantText := make([]byte, len(ciphertext))
-	//解密
-	blockModel.CryptBlocks(plantText, cipherbyte)
-	//去补全码
-	plantText = pkcs7UnPadding(plantText, block.BlockSize())
-	return plantText, nil
-}
 
-func pkcs7UnPadding(plantText []byte, blockSize int) []byte {
-	length := len(plantText)
-	unpadding := int(plantText[length-1])
-	return plantText[:(length - unpadding)]
+	pt := make([]byte, len(ciphertext))
+	c.Decrypt(pt, ciphertext)
+
+	return string(pt[:]), nil
+
 }
