7e25d013fb85b4539a46e227f219c6779876d3b4
ice <ibibin@sina.com>
2023-05-12 08:41:09 +0800

    aes

--- secret/aes.go
+++ secret/aes.go
@@ -1,35 +1,55 @@
 package secret
 
 import (
+	"bytes"
 	"crypto/aes"
-	"encoding/hex"
+	"crypto/cipher"
+	"encoding/base64"
 )
 
-func EncryptAES(key []byte, plaintext string) (string, error) {
-
-	c, err := aes.NewCipher(key)
-	if err != nil {
-		return "", err
-	}
-
-	out := make([]byte, len(plaintext))
-
-	c.Encrypt(out, []byte(plaintext))
-
-	return hex.EncodeToString(out), nil
+func pKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
 }
 
-func DecryptAES(key []byte, ct string) (string, error) {
-	ciphertext, _ := hex.DecodeString(ct)
+func pKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
 
-	c, err := aes.NewCipher(key)
+// 加密
+func AesEncrypt(origData, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
 
-	pt := make([]byte, len(ciphertext))
-	c.Decrypt(pt, ciphertext)
+	blockSize := block.BlockSize()
+	origData = pKCS5Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
 
-	return string(pt[:]), nil
+	return base64.StdEncoding.EncodeToString(crypted), nil
+}
 
+// 解密
+func AesDecrypt(pass64 string, key []byte) ([]byte, error) {
+	crypted, err := base64.StdEncoding.DecodeString(pass64)
+	if err != nil {
+		return nil, err
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData = pKCS5UnPadding(origData)
+	return origData, nil
 }
