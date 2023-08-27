9f2cd82cac9bb4f8f6ff09e3e1c28f285852dce0
ice <ibibin@sina.com>
2023-08-27 15:34:20 +0800

    resp pool

--- resp/resp.go
+++ resp/resp.go
@@ -1,7 +1,34 @@
 package resp
 
+import (
+	"sync"
+)
+
 type Resp struct {
 	Code int    `json:"code"`
 	Msg  string `json:"msg"`
 	Data any    `json:"data,omitempty"`
+}
+
+var respPool = sync.Pool{
+	New: func() any {
+		return &Resp{}
+	},
+}
+
+func Response(code int, msg string, data any) Resp {
+	resp := respPool.Get().(*Resp)
+	defer resp.put()
+	resp.Code = code
+	resp.Msg = msg
+	resp.Data = data
+
+	return *resp
+}
+
+func (resp Resp) put() {
+	resp.Code = 0
+	resp.Msg = ""
+	resp.Data = nil
+	respPool.Put(&resp)
 }
