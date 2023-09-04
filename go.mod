6487861b6f7bd932ba590c88063e1526ee7f0fce
ice <ibibin@sina.com>
2023-09-04 09:16:16 +0800

    resp

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/samber/lo v1.38.1
+	google.golang.org/grpc v1.57.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gen v0.3.23
 	gorm.io/gorm v1.25.2
@@ -11,12 +12,15 @@ require (
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gorm.io/datatypes v1.1.1-0.20230130040222-c43177d3cf8c // indirect
 	gorm.io/hints v1.1.0 // indirect
 	gorm.io/plugin/dbresolver v1.3.0 // indirect
--- resp/resp.go
+++ resp/resp.go
@@ -1,7 +1,10 @@
 package resp
 
 import (
+	"net/http"
 	"sync"
+
+	"google.golang.org/grpc/status"
 )
 
 type response struct {
@@ -25,16 +28,28 @@ func Fail(code int, msg string) response {
 	return *resp
 }
 
-func Ok(code int, msg string) response {
+func Resp(err error) response {
 	resp := respPool.Get().(*response)
 	defer resp.put()
-	resp.Code = code
-	resp.Msg = msg
+	errno := unpack(err)
+	resp.Code = int(errno.Code())
+	resp.Msg = errno.Message()
 
 	return *resp
 }
 
-func OkWithData(code int, msg string, data any) response {
+func RespWithData(data any, err error) response {
+	resp := respPool.Get().(*response)
+	defer resp.put()
+	errno := unpack(err)
+	resp.Code = int(errno.Code())
+	resp.Msg = errno.Message()
+	resp.Data = data
+
+	return *resp
+}
+
+func RespWithCode(code int, msg string, data any) response {
 	resp := respPool.Get().(*response)
 	defer resp.put()
 	resp.Code = code
@@ -43,6 +58,31 @@ func OkWithData(code int, msg string, data any) response {
 
 	return *resp
 }
+
+func unpack(err error) *status.Status {
+	if err != nil {
+		return status.New(http.StatusOK, "ok")
+	}
+	if st, ok := status.FromError(err); ok {
+		return st
+	}
+
+	return status.New(http.StatusInternalServerError, "Internal Server Error")
+}
+
+// func Resp(data any, err error) response {
+// 	if err == nil {
+// 		// httpx.OkJsonCtx(r.Context(), w, resp.OkWithData(http.StatusOK, "ok", response))
+// 		return Ok()
+// 	}
+
+// 	code := int(http.StatusInternalServerError)
+// 	msg := err.Error()
+// 	if st, ok := status.FromError(err); ok {
+// 		code = int(st.Code())
+// 		msg = st.Message()
+// 	}
+// }
 
 func (resp response) put() {
 	resp.Code = 0
